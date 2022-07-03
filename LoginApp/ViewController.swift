4b3c1bf80b324999bdc8ccfabb05e9db31a4b7f5
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2022-07-03 12:21:35 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Lera Savchenko on 3.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
